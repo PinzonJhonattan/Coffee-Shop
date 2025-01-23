0da200bca61a5dcc2d297ecfe8713030c673b4fa
Jhonattan Pinzon <pinzonrodriguezjhonattandanilo@gmail.com>
2025-01-22 22:45:30 -0500

    Un buen trabajo

---
+++ products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.TextField(max_length=200, verbose_name="nombre")
+    description = models.TextField(max_length=300, verbose_name="description")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="pricio")
+    available = models.BooleanField(default=True, verbose_name="disponible")
+    photo = models.ImageField(upload_to="logos", null=True, blank=True, verbose_name="foto")
+    create_at = models.DateTimeField(auto_now_add=True, null=True)
+    
+    def __str__(self):
+        return self.name
